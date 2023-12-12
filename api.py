51e36da763bf00a006787ee5c13695710fcb9098
anair123 <aashishnair8925@gmail.com>
2023-12-11 23:05:59 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,12 +1,40 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import tensorflow as tf
+import transformers
 from transformers import DistilBertTokenizer
 
 app = FastAPI()
 
 # Load the TensorFlow model and tokenizer
 model_path = 'Model/model.h5'
-loaded_model = tf.keras.models.load_model(model_path)
+loaded_model = tf.keras.models.load_model(model_path,
+                                          custom_objects={"TFBertModel": transformers.TFBertModel})
 tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
 max_length = 512
+
+# set the api to take text input
+class PredictionInput(BaseModel):
+    text: str
+
+
+# make classification based on given text
+def predict_fraudulence(text: str):
+    tokenized_sentence = tokenizer.encode_plus(
+        text,
+        truncation=True,
+        padding='max_length',
+        max_length=max_length,
+        return_tensors='tf'
+    )
+    input_ids = tokenized_sentence['input_ids']
+    attention_mask = tokenized_sentence['attention_mask']
+    predictions = loaded_model.predict([input_ids, attention_mask])
+    predicted_label = "Fraudulent" if predictions[0][0] > 0.5 else "Not Fraudulent"
+    return predicted_label
+
+# FastAPI endpoint
+@app.post("/predict")
+async def predict_text(input_data: PredictionInput):
+    predicted_label = predict_fraudulence(input_data.text)
+    return {"prediction": predicted_label}
---
+++ use_api.py
@@ -0,0 +1,16 @@
+import requests
+
+api_endpoint = 'http://127.0.0.1:8000/predict/'
+
+input_text = "This is a good job for you. Trust me...."
+
+payload = {"text": input_text}
+response = requests.post(api_endpoint, json=payload)
+
+if response.status_code == 200:
+    print(response.json())
+else:
+    print("Prediction failed")
+
+
+
